04eb484de819eb0d3b83e0fe372bec8e757b7620
KR-16 <keerthirajkv2@gmail.com>
2023-09-08 02:19:02 -0500

    Navbar Mobile View

--- portfolio/src/components/Navbar.jsx
+++ portfolio/src/components/Navbar.jsx
@@ -73,39 +73,39 @@ const Navbar = () => {
                 to set the FaBar, 
                 if the screen size is less the mid, 
                 then FaBar is set to True and onClick it will display the Menu */}
-                <ul className={!nav ? 'hidden' : 'absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center'}>
+                <ul className={!nav ? 'hidden' : 'absolute top-0 left-0 w-full h-auto bg-[#0a192f] flex flex-col justify-center items-center'}>
                     {/* List Items <li></li> */}
-                    <li className='py-6 text-4xl'>
+                    <li className='py-4 text-4xl'>
                     <Link onClick={handleClick} to="home" smooth={true} offset={50} duration={500}>
                         Home
                     </Link>
                     </li>
-                    <li className='py-6 text-4xl'>
+                    <li className='py-4 text-4xl'>
                     <Link onClick={handleClick} to="about" smooth={true} offset={50} duration={500}>
                         About
                     </Link>
                     </li>
-                    <li className='py-6 text-4xl'>
+                    <li className='py-4 text-4xl'>
                     <Link onClick={handleClick} to="education" smooth={true} offset={50} duration={500}>
                         Education
                     </Link>
                     </li>
-                    <li className='py-6 text-4xl'>
+                    <li className='py-4 text-4xl'>
                     <Link onClick={handleClick} to="skills" smooth={true} offset={50} duration={500}>
                         Skills
                     </Link>
                     </li>
-                    <li className='py-6 text-4xl'>
+                    <li className='py-4 text-4xl'>
                     <Link onClick={handleClick} to="experience" smooth={true} offset={50} duration={500}>
                         Projects
                     </Link>
                     </li>
-                    <li className='py-6 text-4xl'>
+                    <li className='py-4 text-4xl'>
                     <Link onClick={handleClick} to="blog" smooth={true} offset={50} duration={500}>
                         Blog
                     </Link>
                     </li>
-                    <li className='py-6 text-4xl'>
+                    <li className='py-4 text-4xl'>
                     <Link onClick={handleClick} to="certificates" smooth={true} offset={50} duration={500}>
                         Certificates
                     </Link>
